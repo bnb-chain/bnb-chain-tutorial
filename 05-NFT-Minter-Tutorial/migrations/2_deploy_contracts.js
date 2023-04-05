0584122a1ac7d0aeca260a4331116bca1c2c1e6e
DylanCkawalec <Dylankawalec@gmail.com>
2023-04-05 16:25:48 -0700

    bnb-react-challenge-add

--- 05-NFT-Minter-Tutorial/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const Minter = artifacts.require("minter");
-
-module.exports = function(deployer) {
-  // deployer.deploy(ConvertLib);
-  // deployer.link(ConvertLib, MetaCoin);
-  // deployer.deploy(MetaCoin);
-};
